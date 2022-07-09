f5233c2882f0bbd83007859f301d429cfdc29e08
HenkSeed <adaner@inbox.ru>
2022-07-09 20:16:30 +0700

    Add Task02 Lecture01

---
+++ Lecture01_Task02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("Введите Ваше имя: "); // Приглашение ввода
+// string? username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+Console.Write("Введите Ваше имя: "); // Приглашение ввода
+string? username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Lectures and Seminars
 
 1. Lecture folder name format is as follows:
-   - LectM_TaskN (for example: Lect01_Task03)
+   - LectureM_TaskN (for example: Lecture01_Task03)
 2. Seminar folder name format is as follows:
-   - SemP_TaskQ (for example: Sem02_Task01)
+   - SeminarP_TaskQ (for example: Seminar02_Task01)
