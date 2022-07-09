5d3e7bc66161b3088f9a84823188747fc9029418
HenkSeed <adaner@inbox.ru>
2022-07-09 20:39:38 +0700

    Add Task03 of Lecture01. Pgm adds 2 numbers given & outputs result

--- Lecture01_Task02/Program.cs
+++ Lecture01_Task02/Program.cs
@@ -1,8 +1,10 @@
-﻿// Console.WriteLine("Введите Ваше имя: "); // Приглашение ввода
+﻿// Variant 01
+// Console.WriteLine("Введите Ваше имя: "); // Приглашение ввода
 // string? username = Console.ReadLine();
 // Console.WriteLine("Привет, ");
 // Console.WriteLine(username);
 
+// Variant 02
 Console.Write("Введите Ваше имя: "); // Приглашение ввода
 string? username = Console.ReadLine();
 Console.Write("Привет, ");
---
+++ Lecture01_Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Program adds two numbers given and outputs the result
+
+// Variant 01
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+// Variant 02
+int numberA = 33;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
