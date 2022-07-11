6a5bc5e46973c88936eb290705fa7fed3aaa4084
HenkSeed <adaner@inbox.ru>
2022-07-11 17:02:17 +0700

    Seminar01_HW_Task02. Compare two whole numbers, output maximum

---
+++ Seminar01_HW_Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿//                Variant I
+// =======================================================
+Console.Write("Введите первое число: ");
+string? firstNumberStr = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+string? secondNumberStr = Console.ReadLine();
+
+if((firstNumberStr != null) && (secondNumberStr != null))
+{
+   int firstNumber = int.Parse(firstNumberStr);
+   int secondNumber = int.Parse(secondNumberStr);
+
+   if(firstNumber < secondNumber)
+   {
+      Console.WriteLine("max = " + secondNumber);
+   }
+   else
+   {
+      Console.WriteLine("max = " + firstNumber);
+      
+   }
+}
+// -------------------------------------------------------
+//                End Variant I
+
